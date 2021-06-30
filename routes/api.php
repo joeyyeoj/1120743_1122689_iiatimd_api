cab7196f493210bbf9df2bc63a3a5a6e41411139
Tycho van Veen <dev@tychovanveen.nl>
2021-06-30 20:21:42 +0200

    git was corrupt geraakt, dus opnieuw een commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\UserController;
+
+Route::post('login', [ApiController::class, 'authenticate']);
+Route::post('register', [ApiController::class, 'register']);
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('logout', [ApiController::class, 'logout']);
+    Route::get('get_user', [ApiController::class, 'get_user']);
+    Route::put('update/{userId}',  [UserController::class, 'update']);
+});
