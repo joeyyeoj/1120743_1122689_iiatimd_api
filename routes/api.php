2fb4f597d03836afc05a0fa12073296a1e9f343e
Joey <s1120743@student.hsleiden.nl>
2021-07-02 15:07:28 +0200

    route aangemaakt op zooi op te halen

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\UserController;
 Route::post('login', [ApiController::class, 'authenticate']);
 Route::post('register', [ApiController::class, 'register']);
 
+Route::get('getContact/{id}', [UserController::class], 'getContactInfo');
+
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('logout', [ApiController::class, 'logout']);
     Route::get('get_user', [ApiController::class, 'get_user']);
