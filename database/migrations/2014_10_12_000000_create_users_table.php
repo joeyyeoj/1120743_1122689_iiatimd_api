a02771e0559b9746704c864b74a7d1240b307128
Joey <s1120743@student.hsleiden.nl>
2021-08-18 21:13:08 +0200

    migration fix?

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('device_id')->nullable();
+        
             $table->id();
+            $table->string('device_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
