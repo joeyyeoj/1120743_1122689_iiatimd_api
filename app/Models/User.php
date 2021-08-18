842c7752028b9060a840c11f19a0af156b462c56
Joey <s1120743@student.hsleiden.nl>
2021-08-18 21:39:35 +0200

    3e keer scheepsrecht?

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
+    public $primarykey = 'id';
+
     /**
      * The attributes that are mass assignable.
      *
