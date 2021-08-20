639ec5f176fbe3c736c0928d720b6bdce0f2c2cd
Joey <s1120743@student.hsleiden.nl>
2021-08-20 16:13:27 +0200

    verjaardag wegschrijven fix?

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -236,7 +236,7 @@ class ApiController extends Controller
             $user->tiktok = $request->tiktok;
         }
         if($request->geboortedatum != ''){
-            $user->geboortedatum = date('d-m-y', strtotime($request->geboortedatum));
+            $user->geboortedatum = Carbon::createFromFormat('Y-m-d', $request->geboortedatum);
         }
 
         
