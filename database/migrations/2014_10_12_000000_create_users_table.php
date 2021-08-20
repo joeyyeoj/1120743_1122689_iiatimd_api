38b7cd57911b069ec105fe1e36aa738f112a8e22
Joey <s1120743@student.hsleiden.nl>
2021-08-20 15:08:32 +0200

    datum fix?

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,7 +30,7 @@ class CreateUsersTable extends Migration
             $table->string('instagram')->default("")->nullable();
             $table->string('linkedin')->default("")->nullable();
             $table->string('tiktok')->default("")->nullable();
-            $table->dateTime('geboortedatum')->default(date("d-m-y"))->nullable();
+            $table->dateTime('geboortedatum')->default("01-01-1970")->nullable();
         });
     }
 
