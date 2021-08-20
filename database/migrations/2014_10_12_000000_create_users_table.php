ff36405487ce34a349bedc9490d8bf624318fe7e
Joey <s1120743@student.hsleiden.nl>
2021-08-20 15:11:51 +0200

    Werkt bijna maar nu staat de tijd er ook ineens bij, nu gefixed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,7 +30,7 @@ class CreateUsersTable extends Migration
             $table->string('instagram')->default("")->nullable();
             $table->string('linkedin')->default("")->nullable();
             $table->string('tiktok')->default("")->nullable();
-            $table->dateTime('geboortedatum')->default("01-01-1970")->nullable();
+            $table->date('geboortedatum')->default("01-01-1970")->nullable();
         });
     }
 
