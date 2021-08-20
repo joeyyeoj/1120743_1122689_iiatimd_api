8da19c821248fca7b27bee3bfa2a194ddf45ab88
Joey <s1120743@student.hsleiden.nl>
2021-08-20 16:25:40 +0200

    geboortedatum wegschrijven fix?

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -236,7 +236,7 @@ class ApiController extends Controller
             $user->tiktok = $request->tiktok;
         }
         if($request->geboortedatum != ''){
-            $user->geboortedatum = Carbon::parse($request->geboortedatum)->format('y-m-d');
+            $user->geboortedatum = $request->geboortedatum;
         }
 
         
